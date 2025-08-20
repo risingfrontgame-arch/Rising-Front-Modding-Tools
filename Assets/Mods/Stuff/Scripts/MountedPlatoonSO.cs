bdd3876e6ae6e6b387120f8aca3344fda69ee6d3
EchoCharly21 <93404141+EchoCharly21@users.noreply.github.com>
2025-08-20 01:39:15 -0400

    Cavalry and Fire Support Stuff

---
+++ Assets/Mods/Stuff/Scripts/MountedPlatoonSO.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Mounted Platoon", menuName = "Mounted Platoon")]
+public class MountedPlatoonSO : ScriptableObject
+{
+    public int price = 100;
+    public Sprite icon;
+    public bool isGreenTeam = true;
+
+    public MountedSoldierInfo[] mountedSoldiers;
+
+    public string ID;
+}
---
+++ Assets/Mods/Stuff/Scripts/MountedSoldierInfo.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Mounted Soldier", menuName = "Mounted Soldier")]
+public class MountedSoldierInfo : ScriptableObject
+{
+    [Header("General Settings")]
+    public float maxSpeed = 7f;
+    public float health = 100f;
+    public float range = 80f;
+    public float fireRate = 1f;
+    public float accuracy = .015f;
+    public float reloadTime = 5f;
+    public float damage = 35f;
+    public int magSize = 5;
+    public bool canDamageArmor = false;
+    public float armorDamage = 10f;
+    public RuntimeAnimatorController soldierAnimator;
+
+    public ParticleSystem impactEffect;
+    public ParticleSystem bloodEffect;
+    public AudioClip[] weaponSounds;
+    public float weaponSoundMaxRange = 200f;
+    public GameObject weaponProjectile;
+    public float projectileSpeed;
+    [Header("Melee Only Settings")]
+    public bool isMelee = false;
+    public float findTargetRange = 81f;
+    public float attackDelay = 0f;
+
+    [Header("Horse Settings")]
+    public Material horseMaterial;
+    public Material horseMaineMaterial;
+    public GameObject[] horseBackGear;
+    public GameObject[] horseHeadGear;
+    [Header("Soldier Settings")]
+    public SkinnedMeshRenderer soldierBody;
+    public GameObject weapon;
+    public GameObject[] headGear;
+    public GameObject[] backGear;
+
+}
